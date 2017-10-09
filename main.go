0b3e7c9b4b8fdac16336e13eb06eb1bac7759f5b
Victor Piolin <victor.piolin@gmail.com>
2017-10-09 16:48:16 -0400

    ok now notification telegram only serve is real purpuse..
    GetTelegram message, and send Telegrame Message.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,28 +10,6 @@ import (
 
 	"github.com/vico1993/Yoshi/notification"
 )
-
-type returnStruct struct {
-	UpdateID int `json:"update_id"`
-	Message  struct {
-		MessageID int `json:"message_id"`
-		From      struct {
-			ID           int    `json:"id"`
-			IsBot        bool   `json:"is_bot"`
-			FirstName    string `json:"first_name"`
-			LastName     string `json:"last_name"`
-			LanguageCode string `json:"language_code"`
-		} `json:"from"`
-		Chat struct {
-			ID        int    `json:"id"`
-			FirstName string `json:"first_name"`
-			LastName  string `json:"last_name"`
-			Type      string `json:"type"`
-		} `json:"chat"`
-		Date int    `json:"date"`
-		Text string `json:"text"`
-	} `json:"message"`
-}
 
 func hundleCommand(cmd string) {
 
@@ -69,19 +46,9 @@ func main() {
 
 		// TODO : Need to find a way to hundle Http Request, and cmdline interface..
 		// TODO : Need to hundle a multiple way of notification ( Messenger, text, vocale... )
-		data := &returnStruct{}
-		jsonErr := json.Unmarshal([]byte(string(body)), data)
-		if jsonErr != nil {
-			log.Fatal("Error json Unmarshal : ", jsonErr)
-		}
+		var cmd = notification.GetTelegramCommand(body)
 
-		if data.Message.From.IsBot == true {
-			log.Fatal("C'est un bot...")
-		}
-
-		var command = data.Message.Text
-
-		hundleCommand(command)
+		hundleCommand(cmd)
 	})
 
 	log.Println("Serving on localhost:3000")
