81d167f6d8e8018a36c33417bf4292a66463948a
Victor Piolin <victor.piolin@gmail.com>
2017-12-02 19:21:07 +0100

    Working with dev.to

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/vico1993/Yoshi/notification"
 
+func main() {
+	notification.SendTelegramMessage("Bonjour", false)
 }
---
+++ source/devTo.go
@@ -0,0 +1,5 @@
+package source
+
+const (
+	baseURL = "https://dev.to"
+)
