fcbe502fb8f9d799f1fc2e9d1f594a44ed14413b
Victor Piolin <victor.piolin@gmail.com>
2017-12-08 18:36:44 +0100

    First try to handle error with configuration file.. first commit for KillYoshi() great ?

--- utils/utils.go
+++ utils/utils.go
@@ -13,18 +13,38 @@ type Config struct {
 	TelegramBotAPI string `json:"telegram_bot_api"`
 }
 
+type kill struct {
+	Message string
+	err     error
+}
+
+func killYoshi(k kill) {
+	fmt.Println(k.Message)
+	os.Exit(1)
+}
+
 // GetConfigData return all parameter of Conf.json
-func GetConfigData() (Config, error) {
+func GetConfigData() Config {
 	file, errOpeningFile := os.Open("conf.json")
 	if errOpeningFile != nil {
-		fmt.Println("An error append when Yoshi try to acces to your conf.json. Please Make sure the file is here.")
+		killData := kill{"An error append when Yoshi try to acces to your conf.json. Please Make sure the file is here.", errOpeningFile}
+		killYoshi(killData)
 	}
 
 	decoder := json.NewDecoder(file)
 	configuration := Config{}
 	err := decoder.Decode(&configuration)
+	if err != nil {
+		killData := kill{"An error append when Yoshi try to acces to your conf.json. Please check if your json is correct ( https://jsonlint.com ) .", err}
+		killYoshi(killData)
+	}
 
-	return configuration, err
+	if configuration.Path == "/Path/to/Go/module/Yoshi" || configuration.TelegramBotAPI == "123456789" || configuration.TelegramChatID == "my_bot_api_key:123456789" {
+		killData := kill{"Please check that your enter your configuration.", err}
+		killYoshi(killData)
+	}
+
+	return configuration
 }
 
 // CheckConfig is here to check if the config file is here, and if all data in it has change.
