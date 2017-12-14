09b6823a7a16512d5ddff1894f7fad895f27870e
Victor Piolin <victor.piolin@gmail.com>
2017-12-14 22:43:22 +0100

    progress cmd.

--- cmd/source.go
+++ cmd/source.go
@@ -10,17 +10,25 @@ import (
 var sourceCmd = &cobra.Command{
 	Use:   "source",
 	Short: "To get in touch",
-	Long:  `Yoshi sent to you, depande on the source you want`,
+	Long:  `Yoshi sent to you your information where you want`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("SOURCE")
 		fmt.Println(args)
+		fmt.Println(cmd.Flags().Args())
+
+		site, _ := cmd.Flags().GetBool("site")
+		destination, _ := cmd.Flags().GetBool("destination")
+
+		fmt.Println("site:", site)
+		fmt.Println("destination:", destination)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(sourceCmd)
 
-	// Exemple Commande : ./Yoshi source https://devTo.com
+	// Exemple Commande : ./Yoshi source https://devTo.com --destination telegram --site google.com
 
-	sourceCmd.Flags().BoolP("site", "s", true, "Done le site de news vous voulez")
+	sourceCmd.Flags().BoolVarP("site", "s", false, "Done le site de news vous voulez")
+	sourceCmd.Flags().BoolVarP("destination", "d", false, "Sur quelle plateforme souhaitez vous être informé")
 }
